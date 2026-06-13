9986a64eb42bdc6b41dc5c47f17eedd10b6f8b9a
Dhairyashil Anil Ghatage <78363024+dhairyashil-G@users.noreply.github.com>
2026-06-13 21:22:34 +0530

    Update scrapbook.ts

--- scrapbook/app/src/types/scrapbook.ts
+++ scrapbook/app/src/types/scrapbook.ts
@@ -1,12 +1,27 @@
-export interface Page {
-  type: string;
-  [key: string]: any;
+export interface CoverPageConfig {
+  type: "cover";
+  title: string;
+  subtitle: string;
+  photos: string[];
 }
+
+export interface LetterPageConfig {
+  type: "letter";
+  heading: string;
+  contentTop: string;
+  image1?: string;
+  contentImgRight?: string;
+  image2?: string;
+  contentImgLeft?: string;
+  contentBottom: string;
+  signature: string;
+}
+
+export type PageConfig = CoverPageConfig | LetterPageConfig;
 
 export interface ScrapbookConfig {
   recipient: {
     name: string;
   };
-
-  pages: Page[];
+  pages: PageConfig[];
 }
